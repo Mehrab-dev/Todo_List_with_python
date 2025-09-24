2236a87b058d41b699ba1fce4ecedf010394864c
mehrab khanmohammadi <programmer.py.mail@gmail.com>
2025-09-24 17:22:35 +0330

    add tesk.json file and create the function (add_task) in utils and test add_task with pytest

--- test_utils.py
+++ test_utils.py
@@ -1,1 +1,15 @@
-ECHO is on.
+import json
+import utils
+
+def test_add_task(tmp_path,monkeypatch) :
+    data_file = tmp_path / "task.json"
+    monkeypatch.setattr(utils,"data_file",str(data_file))
+
+    task = utils.add_task(task_name="Buy Milk")
+
+    assert task["task_name"] == "Buy Milk"
+
+    with open(data_file,"r",encoding="utf-8") as t :
+        tasks = json.load(t)
+    assert len(tasks) == 1
+    assert tasks[0]["task_name"] == "Buy Milk"
--- utils.py
+++ utils.py
@@ -1,1 +1,38 @@
-ECHO is on.
+import json
+import uuid
+import datetime
+import os
+
+data_file = "task.json"
+
+def add_task(task_name,tags=None,description=None,status=None) :
+    id = str(uuid.uuid4())
+    date = str(datetime.date.today())
+    new_data = {
+        "id":id,
+        "task_name" : task_name,
+        "today_is_date" : date,
+        "description" : description if description else None,
+        "status" : status if status else " ",
+        "tags" : tags if tags else " ",
+        "updated" : date
+    }
+
+    if os.path.exists(data_file) :
+        with open(data_file,"r",encoding="utf-8") as a :
+            try :
+                old_data = json.load(a)
+            except json.JSONDecodeError :
+                old_data = []
+    else :
+        old_data = []
+
+    old_data.append(new_data)
+
+    with open(data_file,"w",encoding="utf-8") as n :
+        json.dump(old_data,n,ensure_ascii=False,indent=2)
+
+    return new_data
+
+add_task(task_name="Buy milk")
+
